511cde10e810ab7414ae7a8121a9c93d5baac678
jimboca <jimboca3@gmail.com>
2016-12-19 20:26:16 -0800

    Update habridge install

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ISYHelperHABridge
 Python interface between the UDI ISY and HABridge
+
+
+# Installation
+
+cd /home/pi
+mkdir ihb
+cd ibh
+git clone https://github.com/jimboca/ISYHelperHABridge
+
+cd ISYHelperHABridge
+./install_habridge.sh
+
+Configure the habridge by going to add a Harmony Hub
+http://your_pi_ip_address
+
+./install_ihb.sh
--- install_habridge.sh
+++ install_habridge.sh
@@ -7,13 +7,16 @@ fi
 
 sudo cp habridge.service /lib/systemd/system/
 
-cd /home/pi
+cd /home/pi/hab
+
 if [ ! -d habridge ]; then
     mkdir habridge
 fi
 cd habridge
 if [ ! -f ha-bridge-3.5.1.jar ]; then
     wget https://github.com/bwssytems/ha-bridge/releases/download/v3.5.1/ha-bridge-3.5.1.jar
+    rm -f ha-bridge.jar
+    ln -s ha-bridge-3.5.1.jar ha-bridge.jar
 fi
 
 sudo systemctl --system daemon-reload
