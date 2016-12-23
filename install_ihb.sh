f1ef911b41dfd41ea9542fe38024c54150ec7747
jimboca <jimboca3@gmail.com>
2016-12-22 19:52:50 -0800

    Fix copying service file

--- install_ihb.sh
+++ install_ihb.sh
@@ -5,11 +5,12 @@ if [ -f /lib/systemd/system/ihb.service ]; then
    sudo systemctl stop ihb
 fi
 
+sudo cp ihb.service /lib/systemd/system/
+sudo systemctl --system daemon-reload
+
 cd ..
 git clone https://github.com/jimboca/PyISY
 
-sudo cp ihb.service /lib/systemd/system/
-sudo systemctl --system daemon-reload
 sudo systemctl enable ihb
 sudo systemctl start ihb
 sudo systemctl status ihb
